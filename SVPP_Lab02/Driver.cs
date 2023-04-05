ce9d11964b4f723434c1c27c5543174c190a98b8
IRINA ZHELAKOVICH <42095780+IraZhe@users.noreply.github.com>
2023-04-05 23:55:38 +0300

    Add files via upload

---
+++ SVPP_Lab02/Driver.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace NewLab02
+{
+    public class Driver
+    {
+        public enum GENDER { male, female, variant};
+        public enum COLOREYES { brown, green, blue, gray};
+        int number;
+        char class1;
+        double hgt;
+        string name;
+        string address;
+        GENDER gender;
+        COLOREYES eyes;
+        DateTime dob;
+        DateTime iss;
+        DateTime exp;
+        bool donor;
+        string uriImage;
+
+        public Driver()
+        {
+        }
+      
+        public int Number { get => number; set => number = value; }
+        public double Hgt { get => hgt; set => hgt = value; }
+        public string Name { get => name; set => name = value; }
+        public GENDER Gender { get => gender; set => gender = value; }
+        public COLOREYES Eyes { get => eyes; set => eyes = value; }
+        public DateTime Dob { get => dob; set => dob = value; }
+        public DateTime Iss { get => iss; set => iss = value; }
+        public DateTime Exp { get => exp; set => exp = value; }
+        public bool Donor { get => donor; set => donor = value; }
+        public char Class1 { get => class1; set => class1 = value; }
+        public string Address { get => address; set => address = value; }
+        public string UriImage { get => uriImage; set => uriImage = value; }
+
+        public override string ToString()
+        {
+            return $"№{Number} {Class1} from {Iss} to {Exp}. {Name}, {Gender} Dob({Dob}). {Address}. Height {Hgt}. Eyes {Eyes}. " +
+                $"{(Donor?"Donor":"Not donor")}";
+        }
+    }
+    //public class ImageConverter : IValueConverter
+    //{
+    //    public object Convert(
+    //        object value, Type targetType, object parameter, CultureInfo culture)
+    //    {
+    //        return new BitmapImage(new Uri(value.ToString()));
+    //    }
+
+    //    public object ConvertBack(
+    //        object value, Type targetType, object parameter, CultureInfo culture)
+    //    {
+    //        throw new NotSupportedException();
+    //    }
+    //}
+}
---
+++ SVPP_Lab02/MainWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace NewLab02
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Driver driver = new Driver();
+        public MainWindow()
+        {
+            InitializeComponent();
+            foreach(Driver.COLOREYES color in Enum.GetValues(typeof(Driver.COLOREYES)))
+            {
+                comboBoxEyes.Items.Add(color.ToString());
+            }
+
+        }
+
+        private void ButtonSave_Click(object sender, RoutedEventArgs e)
+        {
+            int a;
+            Int32.TryParse(textBoxNumber.Text,out a);
+            driver.Number = a;
+            if (textBoxClass.Text.Length > 0)
+                driver.Class1 = textBoxClass.Text[0];
+            else
+                driver.Class1 = 'A';
+            driver.Address = textBoxAddress.Text;
+            if (dataPickerDOB.SelectedDate == null)
+                driver.Dob = DateTime.Now;
+             else
+                driver.Dob = (DateTime)(dataPickerDOB.SelectedDate);
+
+            driver.Gender = Driver.GENDER.variant;
+            if (radioButtonMale.IsChecked == true)
+                driver.Gender = Driver.GENDER.male;
+            if (radioButtonFemale.IsChecked == true)
+                driver.Gender = Driver.GENDER.female;
+            if(comboBoxEyes.SelectedIndex > -1)
+                driver.Eyes = (Driver.COLOREYES)comboBoxEyes.SelectedIndex;
+            driver.Hgt = sliderHGT.Value;
+            MessageBox.Show(driver.ToString());
+        }
+
+        private void Image_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Файлы (jpg)|*.jpg|Все файлы|*.*";
+                
+            if (dialog.ShowDialog() == true)
+            {
+                driver.UriImage = dialog.FileName;
+                image.Source = new BitmapImage(new Uri(driver.UriImage));
+            }
+        }
+
+        private void ButtonLoad_Click(object sender, RoutedEventArgs e)
+        {
+            driver = new Driver()
+            {
+                Name = "Severus Snape",
+                Class1 = 'A',
+                Address = "Hogwarts",
+                Number = 0123456789,
+                Hgt = 192,
+                Gender = Driver.GENDER.male,
+                Eyes = Driver.COLOREYES.gray,
+                Dob = new DateTime(1968, 5, 1),
+                Iss = new DateTime(2008, 10, 22),
+                Exp = new DateTime(2038, 10, 22),
+                Donor = true,
+                UriImage = "Images/Северус Снегг.jpg"
+            };
+            textBoxName.Text = driver.Name;
+            textBoxNumber.Text = driver.Number.ToString();
+            dataPickerDOB.SelectedDate = driver.Dob;
+          //  image.Source = new BitmapImage(new Uri(driver.UriImage));
+
+
+
+        }
+
+        private void ButtonClear_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
